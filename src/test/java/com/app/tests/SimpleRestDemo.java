5238eefa95bdbe61c293860ce36dfa23f8d02510
AihemaitijiangYusufu <yuraphd@gmail.com>
2019-08-06 09:33:22 -0400

    rest assured

---
+++ src/test/java/com/app/tests/SimpleRestDemo.java
@@ -0,0 +1,35 @@
+package com.app.tests;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SimpleRestDemo {
+
+
+    @Test
+    public void test1(){
+
+        //this is class from rest assured library, it contains methods and objects for
+       //creating and sending requests
+        //get--> method for making a get request,accepts the resources url as parameter
+        //response --> represents the response that came back from server based on out request
+
+       Response response = RestAssured.get("https://uinames.com/api/");
+        //print the response
+        response.prettyPrint();
+
+        //asString --> converts the response to string
+        String resString=response.asString();
+        Assert.assertTrue(resString.contains("name"));
+
+        // statusCode returns the response
+        int code=response.statusCode();
+
+        //The HTTP 200 OK success status response code indicates that the request has succeeded.
+        Assert.assertEquals(200,code);
+
+
+    }
+}
