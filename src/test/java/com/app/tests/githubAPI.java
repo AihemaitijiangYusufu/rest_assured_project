d604c3bd7c22072f7ef3467bdead410f3a8bad69
AihemaitijiangYusufu <yuraphd@gmail.com>
2019-08-07 08:46:45 -0400

    githubAPI

---
+++ src/test/java/com/app/tests/githubAPI.java
@@ -0,0 +1,24 @@
+package com.app.tests;
+
+import io.restassured.RestAssured;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static io.restassured.RestAssured.given;
+
+public class githubAPI {
+
+    @BeforeClass
+    public static void setUp(){
+
+        RestAssured.baseURI="https://api.github.com";
+    }
+
+    @Test
+    public void userTest(){
+        //given-->prepare our request
+        //submit-->submit it to url
+        given().pathParam("username","aihemaitijiangyusufu").
+                when().get("/users/{username}").prettyPrint();
+
+    }
+}
