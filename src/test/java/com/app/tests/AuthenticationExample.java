5cff4c4bbc990e1419c3121418cef02717f357f0
AihemaitijiangYusufu <yuraphd@gmail.com>
2019-08-08 08:51:39 -0400

    new test

--- src/test/java/com/app/tests/AuthenticationExample.java
+++ src/test/java/com/app/tests/AuthenticationExample.java
@@ -19,4 +19,39 @@ public class AuthenticationExample {
                 then().log().all().statusCode(200);
 
     }
+
+
+    @Test
+    public void basincAuthentication401Validation(){
+        // https://the-internet.herokuapp.com/basic_auth
+      // when we try to hit and endpoint without being authorised
+       // we can 401 not authorized error
+        RestAssured.given().log().all().
+                when().get("https://the-internet.herokuapp.com/basic_auth").
+                then().log().all().statusCode(401);
+
+    }
+
+    @Test
+    public void basicAuthentication(){
+
+        // auth --> provides different types of authentication
+       // based -->authentication using username and password
+
+        RestAssured.given().
+                auth().basic("admin","admin").
+                when().get("https://the-internet.herokuapp.com/basic_auth").
+                then().log().all().statusCode(200);
+    }
+
+    @Test
+    public void xmlResponseExample(){
+
+        // this is not an authorization example
+        // this method is to show request which returns xml body
+
+        RestAssured.get("http://parabank.parasoft.com/parabank/services/bank/customers/12212").
+                then().log().all().statusCode(200);
+
+    }
 }
